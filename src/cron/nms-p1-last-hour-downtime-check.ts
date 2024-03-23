bb31fc17e5f8de041a9b8c9b992c24d8e7ee1120
Mohammed S <shoaibmohammed92@gmail.com>
2024-03-23 21:20:32 +0530

    query fixes

--- src/cron/nms-p1-last-hour-downtime-check.ts
+++ src/cron/nms-p1-last-hour-downtime-check.ts
@@ -5,8 +5,8 @@ import { EnvVars } from '../env-vars';
 
 const prisma = new PrismaClient();
 
-async function lastHourDowntimeCheck() {
-  console.log('Running NMS P1 Last One Hour Downtime Check...');
+async function lastHourDowntimeP0ChainsCheck() {
+  console.log('Running NMS Last One Hour Downtime P0 Chains Check...');
 
   const rawData = (await prisma.$queryRaw`
     WITH LastHourRequests AS (
@@ -19,7 +19,123 @@ async function lastHourDowntimeCheck() {
       WHERE
         "createdAt" >= NOW() - INTERVAL '1 hour'
         AND type = 'nms'
-        AND priority = 1
+        AND priority = 0
+        AND "chainPriority" = 0
+      GROUP BY
+        chain_id
+    ),
+    DowntimeHourIndicator AS (
+      SELECT
+        chain_id,
+        total_requests,
+        failed_requests,
+        (failed_requests::FLOAT / total_requests) * 100 AS failure_rate_percent,
+        CASE
+          WHEN (failed_requests::FLOAT / total_requests) * 100 > 5 THEN 'Yes'
+          ELSE 'No'
+        END AS is_downtime_hour
+      FROM
+        LastHourRequests
+    )
+    SELECT
+      chain_id,
+      total_requests,
+      failed_requests,
+      failure_rate_percent,
+      is_downtime_hour
+    FROM
+      DowntimeHourIndicator
+    ORDER BY
+      CASE WHEN is_downtime_hour = 'Yes' THEN 1 ELSE 2 END, chain_id;
+  `) as any[];
+
+  if (rawData.some((d) => d.is_downtime_hour === 'Yes')) {
+    const message = {
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: '*NMS P0 Chains Last One Hour Downtime Alert*',
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'At least one chain has experienced downtime in the last hour. Immediate attention required.',
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `For more details, see the <https://metabase.leapwallet.io/question/288-d-NMS-last-one-hour-downtime-p0-chains-check|report>.`,
+          },
+        },
+      ],
+      attachments: [
+        {
+          color: '#FFA1A1', // Light red color
+          blocks: [
+            {
+              type: 'context',
+              elements: [
+                {
+                  type: 'mrkdwn',
+                  text: ':information_source: Please review the report and take necessary actions.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    const slackChannelUrl = EnvVars.getSlackChannelUrl();
+
+    if (!slackChannelUrl) {
+      console.log('Slack channel URL not configured. Skipping Slack notification.');
+      return; // Exit the function if the Slack URL is null
+    }
+
+    try {
+      console.log('Env: ' + EnvVars.getNodeEnv());
+      if (EnvVars.getNodeEnv() === 'prod') {
+        const response = await axios.post(slackChannelUrl, message, {
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.status !== 200) {
+          console.error('Failed to send message to Slack:', response.data);
+        } else {
+          console.log('Downtime alert sent to Slack successfully.');
+        }
+      }
+    } catch (error: any) {
+      console.error('Failed to send message to Slack:', error.response ? error.response.data : error.message);
+    }
+  } else {
+    console.log('No NMS downtime detected in the last hour.');
+  }
+}
+
+async function lastHourDowntimeP1ChainsCheck() {
+  console.log('Running NMS Last One Hour Downtime P1 Chains Check...');
+
+  const rawData = (await prisma.$queryRaw`
+    WITH LastHourRequests AS (
+      SELECT
+        chain_id,
+        COUNT(*) AS total_requests,
+        SUM(CASE WHEN "httpResponseCode" != 200 THEN 1 ELSE 0 END) AS failed_requests
+      FROM
+        response_codes
+      WHERE
+        "createdAt" >= NOW() - INTERVAL '1 hour'
+        AND type = 'nms'
+        AND priority = 0
+        AND "chainPriority" = 1
       GROUP BY
         chain_id
     ),
@@ -45,7 +161,7 @@ async function lastHourDowntimeCheck() {
     FROM
       DowntimeHourIndicator
     ORDER BY
-      chain_id;
+      CASE WHEN is_downtime_hour = 'Yes' THEN 1 ELSE 2 END, chain_id;
   `) as any[];
 
   if (rawData.some((d) => d.is_downtime_hour === 'Yes')) {
@@ -55,7 +171,7 @@ async function lastHourDowntimeCheck() {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: '*NMS P1 Last One Hour Downtime Alert*',
+            text: '*NMS P1 Chains Last One Hour Downtime Alert*',
           },
         },
         {
@@ -69,7 +185,7 @@ async function lastHourDowntimeCheck() {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `For more details, see the <https://metabase.leapwallet.io/question/287-d-nms-p1-last-one-hour-downtime-check|report>.`,
+            text: `For more details, see the <https://metabase.leapwallet.io/question/291-d-NMS-last-one-hour-downtime-p1-chains-check|report>.`,
           },
         },
       ],
@@ -91,15 +207,16 @@ async function lastHourDowntimeCheck() {
       ],
     };
 
+    const slackChannelUrl = EnvVars.getSlackChannelUrl();
+
+    if (!slackChannelUrl) {
+      console.log('Slack channel URL not configured. Skipping Slack notification.');
+      return; // Exit the function if the Slack URL is null
+    }
+
     try {
       console.log('Env: ' + EnvVars.getNodeEnv());
       if (EnvVars.getNodeEnv() === 'prod') {
-        const slackChannelUrl = EnvVars.getSlackChannelUrl();
-
-        if (!slackChannelUrl) {
-          console.log('Slack channel URL not configured. Skipping Slack notification.');
-          return; // Exit the function if the Slack URL is null
-        }
         const response = await axios.post(slackChannelUrl, message, {
           headers: { 'Content-Type': 'application/json' },
         });
@@ -120,9 +237,13 @@ async function lastHourDowntimeCheck() {
 
 // Schedule the task to run every hour
 cron.schedule('0 * * * *', () => {
-  console.log('Running scheduled task for NMS P1 Last One Hour Downtime Check...');
-  lastHourDowntimeCheck();
+  console.log('Running scheduled task for NMS Last One Hour Downtime P0 Chains Check...');
+  lastHourDowntimeP0ChainsCheck();
+
+  console.log('Running scheduled task for NMS Last One Hour Downtime P1 Chains Check...');
+  lastHourDowntimeP1ChainsCheck();
 });
 
 // Initial call
-lastHourDowntimeCheck();
+lastHourDowntimeP0ChainsCheck();
+lastHourDowntimeP1ChainsCheck();
--- src/cron/numia-last-hour-downtime-check.ts
+++ src/cron/numia-last-hour-downtime-check.ts
@@ -5,8 +5,8 @@ import { EnvVars } from '../env-vars';
 
 const prisma = new PrismaClient();
 
-async function lastHourDowntimeCheck() {
-  console.log('Running Numia Last One Hour Downtime Check...');
+async function lastHourDowntimeP0ChainsCheck() {
+  console.log('Running Numia Last One Hour Downtime P0 Chains Check...');
 
   const rawData = (await prisma.$queryRaw`
     WITH LastHourRequests AS (
@@ -19,6 +19,121 @@ async function lastHourDowntimeCheck() {
       WHERE
         "createdAt" >= NOW() - INTERVAL '1 hour'
         AND provider = 'numia'
+        AND "chainPriority" = 0
+      GROUP BY
+        chain_id
+    ),
+    DowntimeHourIndicator AS (
+      SELECT
+        chain_id,
+        total_requests,
+        failed_requests,
+        (failed_requests::FLOAT / total_requests) * 100 AS failure_rate_percent,
+        CASE
+          WHEN (failed_requests::FLOAT / total_requests) * 100 > 5 THEN 'Yes'
+          ELSE 'No'
+        END AS is_downtime_hour
+      FROM
+        LastHourRequests
+    )
+    SELECT
+      chain_id,
+      total_requests,
+      failed_requests,
+      failure_rate_percent,
+      is_downtime_hour
+    FROM
+      DowntimeHourIndicator
+    ORDER BY
+      CASE WHEN is_downtime_hour = 'Yes' THEN 1 ELSE 2 END, chain_id;
+  `) as any[];
+
+  if (rawData.some((d) => d.is_downtime_hour === 'Yes')) {
+    const message = {
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: '*Numia P0 Chains Last One Hour Downtime Alert*',
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'At least one chain has experienced downtime in the last hour. Immediate attention required.',
+          },
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `For more details, see the <https://metabase.leapwallet.io/question/288-d-numia-last-one-hour-downtime-p0-chains-check|report>.`,
+          },
+        },
+      ],
+      attachments: [
+        {
+          color: '#FFA1A1', // Light red color
+          blocks: [
+            {
+              type: 'context',
+              elements: [
+                {
+                  type: 'mrkdwn',
+                  text: ':information_source: Please review the report and take necessary actions.',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    const slackChannelUrl = EnvVars.getSlackChannelUrl();
+
+    if (!slackChannelUrl) {
+      console.log('Slack channel URL not configured. Skipping Slack notification.');
+      return; // Exit the function if the Slack URL is null
+    }
+
+    try {
+      console.log('Env: ' + EnvVars.getNodeEnv());
+      if (EnvVars.getNodeEnv() === 'prod') {
+        const response = await axios.post(slackChannelUrl, message, {
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.status !== 200) {
+          console.error('Failed to send message to Slack:', response.data);
+        } else {
+          console.log('Downtime alert sent to Slack successfully.');
+        }
+      }
+    } catch (error: any) {
+      console.error('Failed to send message to Slack:', error.response ? error.response.data : error.message);
+    }
+  } else {
+    console.log('No Numia downtime detected in the last hour.');
+  }
+}
+
+async function lastHourDowntimeP1ChainsCheck() {
+  console.log('Running Numia Last One Hour Downtime P1 Chains Check...');
+
+  const rawData = (await prisma.$queryRaw`
+    WITH LastHourRequests AS (
+      SELECT
+        chain_id,
+        COUNT(*) AS total_requests,
+        SUM(CASE WHEN "httpResponseCode" != 200 THEN 1 ELSE 0 END) AS failed_requests
+      FROM
+        response_codes
+      WHERE
+        "createdAt" >= NOW() - INTERVAL '1 hour'
+        AND provider = 'numia'
+        AND "chainPriority" = 0
       GROUP BY
         chain_id
     ),
@@ -44,7 +159,7 @@ async function lastHourDowntimeCheck() {
     FROM
       DowntimeHourIndicator
     ORDER BY
-      chain_id;
+      CASE WHEN is_downtime_hour = 'Yes' THEN 1 ELSE 2 END, chain_id;
   `) as any[];
 
   if (rawData.some((d) => d.is_downtime_hour === 'Yes')) {
@@ -54,7 +169,7 @@ async function lastHourDowntimeCheck() {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: '*Numia Last One Hour Downtime Alert*',
+            text: '*Numia P1 Chains Last One Hour Downtime Alert*',
           },
         },
         {
@@ -68,7 +183,7 @@ async function lastHourDowntimeCheck() {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `For more details, see the <https://metabase.leapwallet.io/question/288-d-numia-last-one-hour-downtime-check|report>.`,
+            text: `For more details, see the <https://metabase.leapwallet.io/question/291-d-numia-last-one-hour-downtime-p1-chains-check|report>.`,
           },
         },
       ],
@@ -90,15 +205,16 @@ async function lastHourDowntimeCheck() {
       ],
     };
 
+    const slackChannelUrl = EnvVars.getSlackChannelUrl();
+
+    if (!slackChannelUrl) {
+      console.log('Slack channel URL not configured. Skipping Slack notification.');
+      return; // Exit the function if the Slack URL is null
+    }
+
     try {
       console.log('Env: ' + EnvVars.getNodeEnv());
       if (EnvVars.getNodeEnv() === 'prod') {
-        const slackChannelUrl = EnvVars.getSlackChannelUrl();
-
-        if (!slackChannelUrl) {
-          console.log('Slack channel URL not configured. Skipping Slack notification.');
-          return; // Exit the function if the Slack URL is null
-        }
         const response = await axios.post(slackChannelUrl, message, {
           headers: { 'Content-Type': 'application/json' },
         });
@@ -119,9 +235,13 @@ async function lastHourDowntimeCheck() {
 
 // Schedule the task to run every hour
 cron.schedule('0 * * * *', () => {
-  console.log('Running scheduled task for Numia Last One Hour Downtime Check...');
-  lastHourDowntimeCheck();
+  console.log('Running scheduled task for Numia Last One Hour Downtime P0 Chains Check...');
+  lastHourDowntimeP0ChainsCheck();
+
+  console.log('Running scheduled task for Numia Last One Hour Downtime P1 Chains Check...');
+  lastHourDowntimeP1ChainsCheck();
 });
 
 // Initial call
-lastHourDowntimeCheck();
+lastHourDowntimeP0ChainsCheck();
+lastHourDowntimeP1ChainsCheck();
