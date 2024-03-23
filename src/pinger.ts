4d1e1a07b45f0522e5973afc5a68a05a9d88652d
Mohammed S <shoaibmohammed92@gmail.com>
2024-03-23 20:30:54 +0530

    merge stage (#51)
    * Update pinger.ts
    
    * Update Dockerfile
    
    * Update pinger.ts
    
    * Update pinger.ts
    
    * fixes
    
    * Update app.ts
    
    * Update app.ts
    
    * Update app.ts
    
    * fixes
    
    * Update app.ts
    
    * Update app.ts
    
    * observatory changes
    
    * axios
    
    * singular paid nodes
    
    * Update singularPaidNodes.json
    
    * Update app.ts
    
    * add provider index
    
    * log improvements
    
    * Update app.ts
    
    * slack alerts
    
    * slack url from env
    
    * fixes , slack from env
    
    * fixes
    
    * fixes
    
    * fixes

--- src/pinger.ts
+++ src/pinger.ts
@@ -10,9 +10,28 @@ export namespace Pinger {
       newType: Types = 'ECOSTAKE',
       chainId: string,
       endpoint = '/cosmos/base/tendermint/v1beta1/blocks/latest',
-      priority = 0,
+      priority = -1,
       provider: string | null = null,
     ): Promise<Prisma.ResponseCodeCreateInput> {
+      const highPriorityChains = [
+        'injective',
+        'cosmos_hub',
+        'osmosis',
+        'celestia',
+        'sei',
+        'nibiru',
+        'dymension',
+        'kujira',
+        'coreum',
+        'axelar',
+        'neutron',
+        'stargaze',
+        'stride',
+      ];
+
+      // Determine the priority based on the chainId
+      const chainPriority = highPriorityChains.includes(chainId) ? 0 : 1;
+
       const chainUrl = url;
 
       let responseCode: number;
@@ -20,7 +39,7 @@ export namespace Pinger {
       const startTime = Date.now();
       try {
         const response = await axios.get(`${url}${endpoint}`, {
-          timeout: 20000
+          timeout: 20000,
         });
         responseCode = response.status;
       } catch (err: any) {
@@ -44,7 +63,8 @@ export namespace Pinger {
         chainId: chainId,
         priority: priority,
         provider: provider,
-        errorMessage: errorMessage
+        errorMessage: errorMessage,
+        chainPriority: chainPriority,
       };
       return data;
     }
